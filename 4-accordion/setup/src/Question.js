1cde292f6741520e627de55e6fad1baf73460c75
grumpy-cat <grumpy-cat@mac>
2021-01-18 16:28:05 +0100

    Complete accordion demo application

--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,28 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const Question = ({ title, info }) => {
+  const [displayInfo, setDisplayInfo] = useState(false);
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button
+          type="button"
+          className="btn"
+          onClick={() =>
+            setDisplayInfo((displayInfo) => {
+              return !displayInfo;
+            })
+          }
+        >
+          {displayInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {displayInfo && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
