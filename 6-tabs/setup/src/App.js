16c1aadee798f2359d15adb7838aaec0af270d4f
grumpy-cat <grumpy-cat@mac>
2021-01-20 21:48:33 +0100

    Build Loading Screen and fetch data

--- 6-tabs/setup/src/App.js
+++ 6-tabs/setup/src/App.js
@@ -1,10 +1,38 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = "https://course-api.com/react-tabs-project";
+
 function App() {
-  return <h2>tabs project setup</h2>
+  const [jobs, setJobs] = useState([]);
+  let dataLoaded = false;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => console.log("Error: ", error));
+
+  if (dataLoaded) {
+    return (
+      <section className="section">
+        <div className="title">
+          <h2>experience</h2>
+          <div className="underline"></div>
+        </div>
+      </section>
+    );
+  } else {
+    return (
+      <section className="section">
+        <div className="title">
+          <h2>Loading ...</h2>
+        </div>
+      </section>
+    );
+  }
 }
 
-export default App
+export default App;
